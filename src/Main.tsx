8e7caf6f420e3a0f1e67fe47ff6bb7606a33912e
Migueliyo <migueliyocolmenero@gmail.com>
2023-06-07 13:53:48 +0200

    Mejorando tabs

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,9 @@
 import { Redirect, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 import { IonApp, IonIcon, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { addCircleOutline, barbellOutline, homeOutline, searchOutline } from 'ionicons/icons';
+import { addCircle, addCircleOutline, barbell, barbellOutline, home, homeOutline, search, searchOutline } from 'ionicons/icons';
 
 import { useAuth } from './auth/AuthProvider';
 
@@ -14,62 +15,61 @@ import NewContent from './pages/NewContent';
 import Challenge from './pages/Challenge';
 
 const Main: React.FC = () => {
-  
   const { user } = useAuth();
+  const [activeTab, setActiveTab] = useState('home');
+
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab);
+  };
 
   return (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/myprofile">
-            <MyProfile />
-          </Route>
-          <Route exact path="/profile/:id">
-            <Profile />
-          </Route>
-          <Route exact path="/explore">
-            <Search />
-          </Route>
-          <Route exact path="/new">
-            <NewContent />
-          </Route>
-          <Route path="/challenge">
-            <Challenge />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="home" href="/home">
-            <IonIcon icon={homeOutline} />
-          </IonTabButton>
-          <IonTabButton tab="explore" href="/explore">
-            <IonIcon icon={searchOutline} />
-          </IonTabButton>
-          <IonTabButton tab="new" href="/new">
-            <IonIcon icon={addCircleOutline} />
-          </IonTabButton>
-          <IonTabButton tab="challenge" href="/challenge">
-            <IonIcon icon={barbellOutline} />
-          </IonTabButton>
-          <IonTabButton tab="myprofile" href="/myprofile">
-          {user && <img alt="tab avatar" src={user.avatar} />}
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
+    <IonApp>
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route exact path="/myprofile">
+              <MyProfile />
+            </Route>
+            <Route exact path="/profile/:id">
+              <Profile />
+            </Route>
+            <Route exact path="/explore">
+              <Search />
+            </Route>
+            <Route exact path="/new">
+              <NewContent />
+            </Route>
+            <Route path="/challenge">
+              <Challenge />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/home" />
+            </Route>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="home" href="/home" onClick={() => handleTabChange('home')}>
+              <IonIcon icon={activeTab === 'home' ? home : homeOutline} />
+            </IonTabButton>
+            <IonTabButton tab="explore" href="/explore" onClick={() => handleTabChange('explore')}>
+              <IonIcon icon={activeTab === 'explore' ? search : searchOutline} />
+            </IonTabButton>
+            <IonTabButton tab="new" href="/new" onClick={() => handleTabChange('new')}>
+              <IonIcon icon={activeTab === 'new' ? addCircle : addCircleOutline} />
+            </IonTabButton>
+            <IonTabButton tab="challenge" href="/challenge" onClick={() => handleTabChange('challenge')}>
+              <IonIcon icon={activeTab === 'challenge' ? barbell : barbellOutline} />
+            </IonTabButton>
+            <IonTabButton tab="myprofile" href="/myprofile" onClick={() => handleTabChange('myprofile')}>
+              {user && <img style={activeTab === 'myprofile' ? {border: "2px solid black"} : {}}  alt="tab avatar" src={user.avatar} />}
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
+    </IonApp>
   );
 };
 
 export default Main;
-
-
-
-
-
